92f7d1b5d43a03f26b468fc9dc5113f50f61679e
MarkHildegard <mark.krause09@hotmail.com>
2025-03-09 14:28:39 +0100

    added a new button

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+let audio = document.getElementById("audio");
+let loopButton = document.getElementById("loop-btn");
+
 function playSong(file, title) {
-    let audio = document.getElementById("audio");
     let songTitle = document.getElementById("song-title");
 
     audio.src = file;
     songTitle.textContent = title;
     audio.play();
+}
+
+// Loop-Modus an/aus schalten
+function toggleLoop() {
+    audio.loop = !audio.loop;
+
+    if (audio.loop) {
+        loopButton.textContent = "🔁 Loop an";
+        loopButton.classList.add("loop-active");
+    } else {
+        loopButton.textContent = "🔁 Loop aus";
+        loopButton.classList.remove("loop-active");
+    }
 }
